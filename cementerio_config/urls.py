9aedac9a448e03a7c7887ccac9bf6486d391f62b
jose garcia <jegarciaveliz8@gmail.com>
2026-02-14 16:05:01 -0600

    Limpieza de URLs para compatibilidad con Cloudinary

--- cementerio_config/urls.py
+++ cementerio_config/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.admin_urls if hasattr(admin.site, 'admin_urls') else admin.site.urls),
+    path('admin/', admin.site.urls),
     path('registros/', include('registros.urls')),
     path('', include('registros.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
