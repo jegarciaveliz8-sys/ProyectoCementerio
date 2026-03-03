5e081cbae3511e1dffb7f89517ae890ecb078a46
jose garcia <jegarciaveliz8@gmail.com>
2026-03-03 12:08:13 -0600

    SISTEMA SANARATE G.I.S. BLINDADO - V6.0 - FOTOS Y GPS OK

--- registros/views.py
+++ registros/views.py
@@ -7,12 +7,21 @@ def mapa_cimenterio(request):
     return render(request, 'registros/mapa.html', {'nichos': nichos})
 
 def datos_nichos_json(request):
-    # Agregamos 'id' para que el mapa pueda crear enlaces directos al Admin
-    nichos = Nicho.objects.all().values(
-        'id', 'codigo', 'nombre_difunto', 'lat', 'lng', 
-        'esta_exhumado', 'monto_arbitrio'
-    )
-    return JsonResponse(list(nichos), safe=False)
+    nichos = Nicho.objects.all()
+    data = []
+    for n in nichos:
+        data.append({
+            'id': n.id,
+            'codigo': n.codigo,
+            'nombre_difunto': n.nombre_difunto,
+            'lat': n.lat,
+            'lng': n.lng,
+            'esta_exhumado': n.esta_exhumado,
+            'monto_arbitrio': float(n.monto_arbitrio),
+            # AQUÍ ESTÁ LA MAGIA: Si tiene foto, manda el link de Cloudinary
+            'foto_url': n.foto_nicho.url if n.foto_nicho else None,
+        })
+    return JsonResponse(data, safe=False)
 
 def imprimir_ficha(request, nicho_id):
     nicho = get_object_or_404(Nicho, id=nicho_id)
